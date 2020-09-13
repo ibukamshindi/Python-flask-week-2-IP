f25110bea11b5e91309335a2416661998219cc21
ibukamshindi <ibukamshindi@gmail.com>
2020-09-13 19:54:34 +0300

    add news articles from the selected news source

--- app/models/news.py
+++ app/models/news.py
@@ -12,3 +12,18 @@ class News:
         self.category = category
         self.language = language
         self.country = country
+
+class Articles:
+    """
+    Articles class to define the articles object
+    """
+
+    def __init__(self, source_id, author, title, description, urlToImage, url, date):
+
+        self.source_id = source_id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.urlToImage = urlToImage
+        self.url = url
+        self.date = date
