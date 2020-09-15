6e8d10608604de7469782861496e62e28d1dbba3
ibukamshindi <ibukamshindi@gmail.com>
2020-09-15 11:10:35 +0300

    move config to root import os update init

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     General configuration parent class
     '''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources/{}?apiKey{}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 
 
